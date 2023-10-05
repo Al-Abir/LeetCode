9f675492c66d4c3a77411d39edace0a99c870a44
Al-Abir <sheikhabir196@gmail.com>
2023-10-05 20:17:52 +0600

    Coding

---
+++ Recursion and backtraking/permutations.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+#include <vector>
+using namespace std;
+class Solution {
+public:
+     vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>> result;
+        backtrack(nums, 0, result);
+        return result;
+    }
+
+private:
+    void backtrack(std::vector<int>& nums, int start, std::vector<std::vector<int>>& result) {
+        if (start == nums.size()) {
+            // If all elements are used, add the current permutation to the result
+            result.push_back(nums);
+            return;
+        }
+
+        for (int i = start; i < nums.size(); i++) {
+            // Swap the current element with the element at index 'start'
+              swap(nums[start], nums[i]);
+
+            // Recursively generate permutations for the remaining elements
+            backtrack(nums, start + 1, result);
+
+            // Backtrack by undoing the swap
+               swap(nums[start], nums[i]);
+        }
+    }
+};
+
+int main() {
+    Solution solution;
+    vector<int> nums = {1, 2, 3};
+     vector<std::vector<int>> output = solution.permute(nums);
+
+    // Print the result
+    for (const auto& permutation : output) {
+         cout << "[";
+        for (int num : permutation) {
+           cout << num << ",";
+        }
+        cout << "], ";
+    }
+
+    return 0;
+}
+
+
+
