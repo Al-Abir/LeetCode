aa55780e84898cfb0a9302209a7536842b7a0a9b
Al-Abir <sheikhabir196@gmail.com>
2023-10-06 11:49:47 +0600

    Coding

--- Array/two.cpp
+++ Array/two.cpp
@@ -1,6 +1,50 @@
-#include<bits/stdc++.h>
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+
 using namespace std;
-int main(){
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> m;
+
+        for (int i = 0; i < nums.size(); i++) {
+            int temp = target - nums[i];
+            if (m.find(temp) != m.end()) {
+                return {m.find(temp)->second, i};
+            }
+            m[nums[i]] = i; // insert into hash table
+        }
+        return {};
+    }
+};
+
+int main() {
+    // Example usage
+    Solution solution;
+    vector<int> nums = {2, 7, 11, 15};
+    int target = 9;
+
+    vector<int> result = solution.twoSum(nums, target);
+
+    if (!result.empty()) {
+        cout <<"["<< result[0] << "," << result[1] <<"]"<< endl;
+    } else {
+        cout << "No solution found." << endl;
+    }
+
+    return 0;
+}
+  //time complexity 0(n);
+//--------------------------------------------------------
+
+
+
+/* #include<bits/stdc++.h>
+   using namespace std;
+
+   int main(){
 
 
     int arr[]={3,2,4};
@@ -11,7 +55,7 @@ int main(){
     for(int i=0;i<size; i++){
         for(int j =i+1; j<size; j++){
             if(arr[i]+arr[j]==target){
-                cout<< i << j;
+                cout<< i <<"position "<< j;
             }
 
         }
@@ -21,3 +65,5 @@ int main(){
 
     return 0;
 }
+ // time complexity n^2;
+*/
